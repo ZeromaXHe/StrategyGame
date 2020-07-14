51b6b9e3a4edc786f15ec1d2fd3849ffb7b42408
Zhu XH <zhuxiaohe95@163.com>
2020-07-14 22:54:10 +0800

    2020/07/14 引入log4j和slf4j日志框架

--- strategy_game_main/src/main/java/com/zerox/controller/GameController.java
+++ strategy_game_main/src/main/java/com/zerox/controller/GameController.java
@@ -1,6 +1,11 @@
 package com.zerox.controller;
 
 import com.zerox.entity.GameMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 游戏控制器
@@ -15,6 +20,9 @@ public class GameController {
     private GameMap gameMap;
 
     public static void main(String[] args) {
-
+        Logger logger = LoggerFactory.getLogger(GameController.class);
+        logger.info("这是GameController发送的info日志消息");
+        logger.warn("这是GameController发送的warn日志消息");
+        logger.error("这是GameController发送的error日志消息 {}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
 }
