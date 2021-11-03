e68a3949bdb9d0dea889cce9762db144cf60a5d1
Zhu XH <zhuxiaohe95@163.com>
2021-11-03 11:17:53 +0800

    新增 demoFxml.fxml，DemoTest 测试类现在使用 FXML

--- strategy_game_main/src/test/java/com/zerox/test/DemoTest.java
+++ strategy_game_main/src/test/java/com/zerox/test/DemoTest.java
@@ -3,14 +3,16 @@ package com.zerox.test;
 import com.zerox.engine.GameLoopTimer;
 import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @Author: zhuxi
@@ -18,68 +20,72 @@ import javafx.stage.Stage;
  * @Description:
  * @ModifiedBy: zhuxi
  */
-public class DemoTest extends Application {
+public class DemoTest extends Application implements Initializable {
+    private SimpleStringProperty text = new SimpleStringProperty("0");
+    private SimpleStringProperty fps = new SimpleStringProperty("0.0");
+    private GameLoopTimer timer = new GameLoopTimer() {
+        @Override
+        public void tick(float secondsSinceLastFrame) {
+            int num = Integer.parseInt(text.get());
+            num++;
+            text.set(String.valueOf(num));
+            fps.set(String.valueOf(1 / secondsSinceLastFrame));
+        }
+    };
+
+    // 貌似 public 的就不需要注解 @FXML？
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextField fpsTextField;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        AnchorPane anchorPane = new AnchorPane();
+        URL resource = getClass().getResource("../fxml/demoFxml.fxml");
+        FXMLLoader loader = new FXMLLoader(resource);
+        Parent root = loader.load();
 
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
-        anchorPane.getChildren().add(vBox);
-        AnchorPane.setTopAnchor(vBox, 30.0);
-        AnchorPane.setLeftAnchor(vBox, 50.0);
+        System.out.println("loader.load()");
 
-        HBox hBox = new HBox();
-        hBox.setSpacing(10);
-        vBox.getChildren().add(hBox);
-
-        SimpleStringProperty text = new SimpleStringProperty("0");
-        SimpleStringProperty fps = new SimpleStringProperty("0.0");
-        GameLoopTimer timer = new GameLoopTimer() {
-            @Override
-            public void tick(float secondsSinceLastFrame) {
-                int num = Integer.parseInt(text.get());
-                num++;
-                text.set(String.valueOf(num));
-                fps.set(String.valueOf(1 / secondsSinceLastFrame));
-            }
-        };
-
-        Button pauseBtn = new Button("pause");
-        pauseBtn.setOnAction(event -> timer.pause());
-        Button playBtn = new Button("play");
-        playBtn.setOnAction(event -> timer.play());
-
-        TextField textField = new TextField();
-        textField.setPrefWidth(100);
-        textField.setEditable(false);
-        textField.textProperty().bind(text);
-
-        hBox.getChildren().addAll(pauseBtn, playBtn, textField);
-
-        HBox fpsHBox = new HBox();
-        fpsHBox.setSpacing(10);
-        vBox.getChildren().add(fpsHBox);
-
-        Label fpsLabel = new Label("fps");
-        TextField fpsTextField = new TextField();
-        fpsTextField.setPrefWidth(100);
-        fpsTextField.setEditable(false);
-        fpsTextField.textProperty().bind(fps);
-
-        fpsHBox.getChildren().addAll(fpsLabel, fpsTextField);
-
-        Scene scene = new Scene(anchorPane);
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Demo");
         primaryStage.setWidth(400);
         primaryStage.setHeight(300);
         primaryStage.show();
 
+        // 这里无效了，不知道为什么。
+        // 放在 initialize() 里面倒是可以
+        // 只好新加一个按钮来开始
+//        timer.start();
+    }
+
+    @FXML
+    public void start() {
         timer.start();
+        System.out.println("timer.start()");
+    }
+
+    @FXML
+    public void pause() {
+        timer.pause();
+        System.out.println("timer.pause()");
+    }
+
+    @FXML
+    public void play() {
+        timer.play();
+        System.out.println("timer.play()");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        textField.textProperty().bind(text);
+        fpsTextField.textProperty().bind(fps);
+        System.out.println("initialize()");
     }
 }
